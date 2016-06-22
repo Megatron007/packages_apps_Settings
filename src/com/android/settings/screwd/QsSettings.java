78521a8f65ea45821e4f4a21c5b7d4e5c80d14d0
LorDClockaN <lordclockan@gmail.com>
2016-06-22 22:15:08 +0200

    Settings: QS shade stroke (2/2)
    Change-Id: I9a773fdcf041f1e4724d771e963369cf1b379ef3
    Signed-off-by: LorDClockaN <lordclockan@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/screwd/QsSettings.java
+++ src/com/android/settings/screwd/QsSettings.java
@@ -46,6 +46,10 @@ public class QsSettings extends SettingsPreferenceFragment implements
 	private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
     private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
+    private static final String PREF_QS_STROKE = "qs_stroke";
+    private static final String PREF_QS_STROKE_COLOR = "qs_stroke_color";
+    private static final String PREF_QS_STROKE_THICKNESS = "qs_stroke_thickness";
+    private static final String PREF_QS_CORNER_RADIUS = "qs_corner_radius";
 
 	private SeekBarPreferenceCham mQSShadeAlpha;
 	private SeekBarPreferenceCham mQSHeaderAlpha;
@@ -53,6 +57,12 @@ public class QsSettings extends SettingsPreferenceFragment implements
 	private ListPreference mTileAnimationStyle;
     private ListPreference mTileAnimationDuration;
 	private ListPreference mTileAnimationInterpolator;
+    private ListPreference mQSStroke;
+    private ColorPickerPreference mQSStrokeColor;
+    private SeekBarPreferenceCham mQSStrokeThickness;
+    private SeekBarPreferenceCham mQSCornerRadius;
+
+	static final int DEFAULT_QS_STROKE_COLOR = 0xFF80CBC4;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -111,6 +121,44 @@ public class QsSettings extends SettingsPreferenceFragment implements
         mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
         updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
         mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
+
+		// QS stroke
+        mQSStroke =
+                (ListPreference) findPreference(PREF_QS_STROKE);
+        int qSStroke = Settings.System.getIntForUser(getContentResolver(),
+                        Settings.System.QS_STROKE, 1,
+                        UserHandle.USER_CURRENT);
+        mQSStroke.setValue(String.valueOf(qSStroke));
+        mQSStroke.setSummary(mQSStroke.getEntry());
+        mQSStroke.setOnPreferenceChangeListener(this);
+
+        // QS stroke color
+        mQSStrokeColor =
+                (ColorPickerPreference) findPreference(PREF_QS_STROKE_COLOR);
+        mQSStrokeColor.setOnPreferenceChangeListener(this);
+        int qSIntColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_STROKE_COLOR, DEFAULT_QS_STROKE_COLOR);
+        String qSHexColor = String.format("#%08x", (0xFF80CBC4 & qSIntColor));
+        mQSStrokeColor.setSummary(qSHexColor);
+        mQSStrokeColor.setNewPreviewColor(qSIntColor);
+
+        // QS stroke thickness
+        mQSStrokeThickness =
+                (SeekBarPreferenceCham) findPreference(PREF_QS_STROKE_THICKNESS);
+        int qSStrokeThickness = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_STROKE_THICKNESS, 4);
+        mQSStrokeThickness.setValue(qSStrokeThickness / 1);
+        mQSStrokeThickness.setOnPreferenceChangeListener(this);
+
+        // QS corner radius
+        mQSCornerRadius =
+                (SeekBarPreferenceCham) findPreference(PREF_QS_CORNER_RADIUS);
+        int qSCornerRadius = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_CORNER_RADIUS, 0);
+        mQSCornerRadius.setValue(qSCornerRadius / 1);
+        mQSCornerRadius.setOnPreferenceChangeListener(this);
+
+        QSSettingsDisabler(qSStroke);
     }
 
     @Override
@@ -156,9 +204,49 @@ public class QsSettings extends SettingsPreferenceFragment implements
                     tileAnimationInterpolator, UserHandle.USER_CURRENT);
             updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
             return true;
+		} else if (preference == mQSStroke) {
+                int qSStroke = Integer.parseInt((String) newValue);
+                int index = mQSStroke.findIndexOfValue((String) newValue);
+                Settings.System.putIntForUser(getContentResolver(), Settings.System.
+                        QS_STROKE, qSStroke, UserHandle.USER_CURRENT);
+                mQSStroke.setSummary(mQSStroke.getEntries()[index]);
+                QSSettingsDisabler(qSStroke);
+                return true;
+            } else if (preference == mQSStrokeColor) {
+                String hex = ColorPickerPreference.convertToARGB(
+                        Integer.valueOf(String.valueOf(newValue)));
+                preference.setSummary(hex);
+                int intHex = ColorPickerPreference.convertToColorInt(hex);
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.QS_STROKE_COLOR, intHex);
+                return true;
+            } else if (preference == mQSStrokeThickness) {
+                int val = (Integer) newValue;
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.QS_STROKE_THICKNESS, val * 1);
+                return true;
+            } else if (preference == mQSCornerRadius) {
+                int val = (Integer) newValue;
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.QS_CORNER_RADIUS, val * 1);
+                return true;
         }
 		return false;
     }
+
+	private void QSSettingsDisabler(int qSStroke) {
+            if (qSStroke == 0) {
+                mQSStrokeColor.setEnabled(false);
+                mQSStrokeThickness.setEnabled(false);
+            } else if (qSStroke == 1) {
+                mQSStrokeColor.setEnabled(false);
+                mQSStrokeThickness.setEnabled(true);
+            } else {
+                mQSStrokeColor.setEnabled(true);
+                mQSStrokeThickness.setEnabled(true);
+            }
+    }
+
 
 	private void updateNumColumnsSummary(int numColumns) {
         String prefix = (String) mNumColumns.getEntries()[mNumColumns.findIndexOfValue(String
