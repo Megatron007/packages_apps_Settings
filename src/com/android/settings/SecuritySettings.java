4552c3f18667f6c0cbcea897b2d1d62a2a026fc3
Max Weninger <max.weninger@gmail.com>
2016-08-06 23:10:53 +0200

    [2/3] Settings: make overlay permission check configurable
    google added security check when changing app permissions
    to check for apps running with enabled draw over apps
    (SYSTEM_ALERT_WINDOW) permission.
    
    Since OmniSwitch is such an app it will always trigger this
    and requires users to remove permissions before changing
    app permissions. After that the permission must be added back.
    
    Since this is a PITA add a config that will disable the check
    This is reducing security against tapjacking so make it
    clear to the user what it means to disable it
    
    TODO: maybe add a even more prominent alert when
    enabling the value
    
    Change-Id: I370a69467eaf1be2063872bbede13693fddeb31a
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -106,6 +106,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String PACKAGE_MIME_TYPE = "application/vnd.android.package-archive";
     private static final String KEY_TRUST_AGENT = "trust_agent";
     private static final String KEY_SCREEN_PINNING = "screen_pinning_settings";
+    private static final String KEY_PACKAGE_INSTALL_OVERLAY_CHECK = "toggle_package_install_overlay_check";
 
     // These switch preferences need special handling since they're not all stored in Settings.
     private static final String SWITCH_PREFERENCE_KEYS[] = { KEY_LOCK_AFTER_TIMEOUT,
@@ -142,6 +143,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
     private Intent mTrustAgentClickIntent;
     private Preference mOwnerInfoPref;
+
+    private SwitchPreference mPackageInstallOverlayCheck;
 
     @Override
     protected int getMetricsCategory() {
@@ -347,6 +350,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
         // smarter in the future.
         Index.getInstance(getActivity())
                 .updateFromClassNameResource(SecuritySettings.class.getName(), true, true);
+
+            mPackageInstallOverlayCheck = (SwitchPreference) findPreference(
+                    KEY_PACKAGE_INSTALL_OVERLAY_CHECK);
 
         for (int i = 0; i < SWITCH_PREFERENCE_KEYS.length; i++) {
             final Preference pref = findPreference(SWITCH_PREFERENCE_KEYS[i]);
@@ -641,6 +647,11 @@ public class SecuritySettings extends SettingsPreferenceFragment
             mResetCredentials.setEnabled(!mKeyStore.isEmpty());
         }
 
+        if (mPackageInstallOverlayCheck != null) {
+            mPackageInstallOverlayCheck.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PACKAGE_INSTALL_OVERLAY_CHECK_DISABLED, 0) != 0);
+        }
+
         updateOwnerInfo();
     }
 
@@ -669,6 +680,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 startActivity(mTrustAgentClickIntent);
                 mTrustAgentClickIntent = null;
             }
+        } else if (KEY_PACKAGE_INSTALL_OVERLAY_CHECK.equals(key)) {
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.PACKAGE_INSTALL_OVERLAY_CHECK_DISABLED,
+                    mPackageInstallOverlayCheck.isChecked() ? 1 : 0);
         } else {
             // If we didn't handle it, let preferences handle it.
             return super.onPreferenceTreeClick(preferenceScreen, preference);
